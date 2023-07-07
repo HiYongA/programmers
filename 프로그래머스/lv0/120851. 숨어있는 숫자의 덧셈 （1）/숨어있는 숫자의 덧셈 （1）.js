ed12b8cd5e4b845c64e0a61e3b61fb94a57878d0
YongA <120562771+HiYongA@users.noreply.github.com>
2023-07-08 00:18:03 +0900

    Update 숨어있는 숫자의 덧셈 （1）.js

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -5,3 +5,29 @@ function solution(my_string) {
     0
   );
 }
+
+// 다른 풀이(1)
+// function solution(my_string) {
+// let answer = 0;
+// const num = my_string.replace(/[^0-9]/g,'')
+
+// for(let i=0; i < num.length; i++) {
+// answer += parseInt(num[i])
+// }
+// return answer
+// }
+
+// 다른 풀이(2)
+// function solution(my_string) {
+//문자열에서 -> for ~ of문을 돌려보자
+
+// let sum = 0;
+
+// for (let x of my_string) {
+// if (!isNaN(Number(x))) {
+// sum += Number(x);
+// }
+// }
+
+// return sum;
+// }
