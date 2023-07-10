6193dfb3b753eea5c30e8530f9d01bc9ebb80261
YongA <120562771+HiYongA@users.noreply.github.com>
2023-07-10 23:49:16 +0900

    [level 0] Title: 가위 바위 보, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,11 +1,11 @@
 function solution(rsp) {
   let answer = "";
   for (const i of rsp) {
-    if (i == "2") {
+    if (i === "2") {
       answer += "0";
-    } else if (i == "0") {
+    } else if (i === "0") {
       answer += "5";
-    } else if (i == "5") {
+    } else if (i === "5") {
       answer += "2";
     }
   }
