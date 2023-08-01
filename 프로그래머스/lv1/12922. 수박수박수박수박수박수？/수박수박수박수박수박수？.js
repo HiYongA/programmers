4435ac48ae48aa231e8b57f7be6efe7e17eac16f
YongA <120562771+HiYongA@users.noreply.github.com>
2023-08-01 09:32:53 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -9,3 +9,10 @@ function solution(n) {
   }
   return answer;
 }
+
+// 다른 풀이
+// repeat(): 문자열을 주어진 횟수만큼 반복해 붙인 새로운 문자열을 반환하는 메서드
+// 짝수인 경우에는 "수박".repeat(n/2)로 반복한 "수박"패턴이 반환되고, 홀수인 경우에는 "수박".repeat(n/2)+"수"로 "수박수"패턴이 반환된다.
+// function solution(n) {
+//   return "수박".repeat(n / 2) + (n % 2 === 0 ? "" : "수");
+// }
