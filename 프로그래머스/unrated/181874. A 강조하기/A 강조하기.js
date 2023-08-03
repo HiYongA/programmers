f874b9ca3f7f61052721f9f961c76478a42eeaab
YongA <120562771+HiYongA@users.noreply.github.com>
2023-08-03 09:47:10 +0900

    [unrated] Title: A 강조하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
@@ -2,3 +2,9 @@ function solution(myString) {
     // 정규표현식인 /a/g는 모든 "a"를 전역으로 검색하고 "a"=> "A"로 바꿔준다.
   return myString.toLowerCase().replace(/a/g, "A"); 
 }
+
+// 다른 풀이
+// const solution = (myString) => {
+// myString을 전부 소문자로 변환 후 myString의 모든 a를 A로 바꿔주고 return
+//  return myString.toLowerCase().replaceAll("a", "A")
+// }
