2d7c0f7088efb2fe5b57c0ebcf59682f3135913c
YongA <120562771+HiYongA@users.noreply.github.com>
2023-10-18 15:03:31 +0900

    [level unrated] Title: 주사위의 개수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/unrated/120845. 주사위의 개수/주사위의 개수.js
@@ -2,3 +2,11 @@ function solution(box, n) {
     const maxCubesInBox = Math.floor(box[0] / n) * Math.floor(box[1] / n) * Math.floor(box[2] / n);
     return maxCubesInBox;
 }
+
+// 다른 사람풀이
+function solution(box, n) {
+    let [width, length, height] = box;
+
+    return Math.floor(width / n) * Math.floor(length / n) * Math.floor(height / n);
+
+}
