8c7265ab47d491941c2923c0fc386ade7130ce45
YongA <120562771+HiYongA@users.noreply.github.com>
2023-10-21 17:41:07 +0900

    [level unrated] Title: 배열 회전시키기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120844. 배열 회전시키기/배열 회전시키기.js
@@ -0,0 +1,11 @@
+function solution(numbers, direction) {
+  if (direction === "right") {
+    const lastElement = numbers.pop(); // 마지막 원소 추출
+    numbers.unshift(lastElement); // 첫 번째 위치에 추가
+  } else if (direction === "left") {
+    const firstElement = numbers.shift(); // 첫 번째 원소 추출
+    numbers.push(firstElement); // 마지막 위치에 추가
+  }
+
+  return numbers;
+}
