da241c5891a5543287ba938bd5418b21d557d901
YongA <120562771+HiYongA@users.noreply.github.com>
2023-12-03 02:50:00 +0900

    [level unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    let answer = 0;
+    for (let i = 1; i <= n; i++) {
+        if (n % 2 === 1) {
+            if (i % 2 === 1) {
+                answer += i;
+            }
+        } else {
+            if (i % 2 === 0) {
+                answer += i * i;
+            }
+        }
+    }
+
+    return answer;
+}
